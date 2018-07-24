13c7a8d99390772b2f7bc11c480d3c69c2022b40
Sanduni93 <nirodhajayaweera1993@gmail.com>
2018-07-24 17:29:05 +0530

    Decide about you

---
+++ Lab5Ex1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class Lab5Ex1{
+	
+	public static void main(String args[]){
+		Scanner scn = new Scanner(System.in);
+		System.out.print("Enter your age:");
+	    int a = scn.nextInt();
+		
+		if(a>18){
+			System.out.println("You are an adult");
+			}else{
+			System.out.println("You are an child");	
+			}
+		
+		
+		
+		
+	}
+}
